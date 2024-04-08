359757b577d43938a54306439262c62c505066db
mblees <104490472+mblees@users.noreply.github.com>
2024-04-08 21:54:15 +0200

    added deep sleep functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,77 @@
 #include "mqtt.h"
 #include "bme280.h"
 #include "lm393_photo_sensor.h"
+#include <time.h>
+
+#define uS_TO_S_FACTOR 1000000 /* Conversion factor for micro seconds to seconds */
+#define mS_TO_S_FACTOR 1000    /* Conversion factor for milliseconds to seconds */
+#define TIME_TO_SLEEP 20        /* Time ESP32 will go to sleep (in seconds) */
+
+RTC_DATA_ATTR int bootCount;
+
+void do_work(void);
 
 void setup()
 {
+  uint16_t start_time = millis();
+
   Serial.begin(9600);
-  
+  Serial.println("Arrived in setup()");
+
   init_mqtt();
   init_bme280();
   init_lm393();
+
+  do_work();
+  
+  Serial.println("Going to sleep now");
+  uint16_t time_awake = millis() - start_time;
+  Serial.println("Total time awake: " + String(time_awake) + "ms");
+
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR - (time_awake * mS_TO_S_FACTOR));
+  delay(200); // without this the deep sleep doesnt work properly
+  esp_deep_sleep_start();
 }
 
 void loop()
 {
+  Serial.println("Arrived in loop()");
+}
+
+void do_work()
+{
   mqtt_loop();
 
-  // Send Sensor Data every 5 seconds
-  static unsigned long lastMillis = 0;
-  if (millis() - lastMillis > 5000)
-  {
-    lastMillis = millis();
+  bootCount++;
+  Serial.println("Boot number: " + String(bootCount));
 
-    // Read sensor data
-    float temperature = read_temperature();
-    float humidity = read_humidity();
-    float pressure = read_pressure();
-    float altitude = read_altitude();
-    float light_status = read_photo_diode_value();
+  // Read sensor data
+  float temperature = read_temperature();
+  float humidity = read_humidity();
+  float pressure = read_pressure();
+  float altitude = read_altitude();
+  float light_status = read_photo_diode_value();
 
-    // Create a message string to send to Serial Monitor
-    char sensor_data_msg[100];
-    snprintf(sensor_data_msg, 100, "Temperature: %.2f°C, Humidity: %.2f%%, Pressure: %.2fhPa, Altitude: %.2fm, Light Status: %.2f%", temperature, humidity, pressure, altitude, light_status);
-    Serial.println(sensor_data_msg);
+  // Create a message string to send to Serial Monitor
+  char sensor_data_msg[100];
+  snprintf(sensor_data_msg, 100, "Temperature: %.2f°C, Humidity: %.2f%%, Pressure: %.2fhPa, Altitude: %.2fm, Light Status: %.2f%", temperature, humidity, pressure, altitude, light_status);
+  Serial.println(sensor_data_msg);
 
-    // Publish sensor data to MQTT topics
-    char mqtt_msg[10];
-    
-    dtostrf(temperature, 4, 2, mqtt_msg); // Convert float to string
-    send_mqtt_message("esp01/temperature", mqtt_msg);
+  // Publish sensor data to MQTT topics
+  char mqtt_msg[10];
 
-    dtostrf(humidity, 4, 2, mqtt_msg);
-    send_mqtt_message("esp01/humidity", mqtt_msg);
+  dtostrf(temperature, 4, 2, mqtt_msg); // Convert float to string
+  send_mqtt_message("esp01/temperature", mqtt_msg);
 
-    dtostrf(pressure, 6, 2, mqtt_msg);
-    send_mqtt_message("esp01/pressure", mqtt_msg);
+  dtostrf(humidity, 4, 2, mqtt_msg);
+  send_mqtt_message("esp01/humidity", mqtt_msg);
 
-    dtostrf(altitude, 6, 2, mqtt_msg);
-    send_mqtt_message("esp01/altitude", mqtt_msg);
+  dtostrf(pressure, 6, 2, mqtt_msg);
+  send_mqtt_message("esp01/pressure", mqtt_msg);
 
-    dtostrf(light_status, 4, 2, mqtt_msg);
-    send_mqtt_message("esp01/light_status", mqtt_msg);
-  }
+  dtostrf(altitude, 6, 2, mqtt_msg);
+  send_mqtt_message("esp01/altitude", mqtt_msg);
+
+  dtostrf(light_status, 4, 2, mqtt_msg);
+  send_mqtt_message("esp01/light_status", mqtt_msg);
 }
