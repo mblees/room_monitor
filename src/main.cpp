138fc711ede9acc0c8b0bae39b0c853039fccc30
mblees <104490472+mblees@users.noreply.github.com>
2024-04-12 00:16:10 +0200

    fixed bug where sleeptime can become negative

--- src/main.cpp
+++ src/main.cpp
@@ -74,6 +74,10 @@ void publish_sensor_data()
 
   dtostrf(light_status, 4, 2, mqtt_msg);
   send_mqtt_message(MQTT_TOPIC_LIGHT_STATUS, mqtt_msg);
+
+  if(bootCount == 1){
+    send_mqtt_message(MQTT_TOPIC_BOOT_RESET, "1");
+  }
 }
 
 void deep_sleep()
@@ -81,11 +85,16 @@ void deep_sleep()
   uint16_t time_awake = millis() - start_time;
 
   #if DEBUG_ENABLE == 1
-  Serial.println("Going to sleep now");
+    Serial.println("Going to sleep now");
     Serial.println("Total time awake: " + String(time_awake) + "ms");
   #endif
+  
+  uint32_t calculated_sleep_time = TIME_TO_SLEEP * uS_TO_S_FACTOR - (time_awake * mS_TO_S_FACTOR);
+  if(calculated_sleep_time < 0){
+    calculated_sleep_time = TIME_TO_SLEEP * uS_TO_S_FACTOR;
+  }
 
-  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR - (time_awake * mS_TO_S_FACTOR));
+  esp_sleep_enable_timer_wakeup(calculated_sleep_time);
   delay(200); // without this the deep sleep doesnt work properly
   esp_deep_sleep_start();
 }
