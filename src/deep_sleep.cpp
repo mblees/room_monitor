78f8a37480f3e0533fc4f62ad880a718149fcd4d
mblees <104490472+mblees@users.noreply.github.com>
2024-04-15 14:03:47 +0200

    made deep_sleep function more readable

--- src/deep_sleep.cpp
+++ src/deep_sleep.cpp
@@ -1,5 +1,7 @@
 #include "deep_sleep.h"
 
+uint32_t calculate_sleep_time(uint16_t time_awake);
+
 void deep_sleep(uint16_t start_time)
 {
   uint16_t time_awake = millis() - start_time;
@@ -8,13 +10,18 @@ void deep_sleep(uint16_t start_time)
     Serial.println("Going to sleep now");
     Serial.println("Total time awake: " + String(time_awake) + "ms");
   #endif
-  
+
+  esp_sleep_enable_timer_wakeup(calculate_sleep_time(time_awake));
+  delay(200); // without this the deep sleep doesnt work properly
+  esp_deep_sleep_start();
+}
+
+uint32_t calculate_sleep_time(uint16_t time_awake){
+
   uint32_t calculated_sleep_time = TIME_TO_SLEEP * uS_TO_S_FACTOR - (time_awake * mS_TO_S_FACTOR);
   if(calculated_sleep_time < 0){
     calculated_sleep_time = TIME_TO_SLEEP * uS_TO_S_FACTOR;
   }
 
-  esp_sleep_enable_timer_wakeup(calculated_sleep_time);
-  delay(200); // without this the deep sleep doesnt work properly
-  esp_deep_sleep_start();
+  return calculated_sleep_time;
 }
